6f691375289987c00609365151bd6abe5584b984
crossibc <Administrator@iMac.local>
2017-12-11 23:39:17 -0500

    got the moving start game label to work, floats around the screen disappearing and reappearing. clikcing it opens a blank new game scene

--- Dark Maze/Dark Maze/GameScene.swift
+++ Dark Maze/Dark Maze/GameScene.swift
@@ -12,10 +12,11 @@ import GameplayKit
 class GameScene: SKScene {
     
     var floatingStartButton = SKLabelNode()
-    
+
     override func sceneDidLoad() {
         print("scene loaded")
     }
+    
     override func didMove(to view: SKView) {
         
         floatingStartButton = self.childNode(withName: "FloatingStartGameButton") as! SKLabelNode
@@ -23,26 +24,29 @@ class GameScene: SKScene {
             x: CGFloat(arc4random_uniform(UInt32(frame.width - floatingStartButton.frame.width))),
             y: CGFloat(arc4random_uniform(UInt32(frame.height - floatingStartButton.frame.height)))
         )
+        /*let newX = arc4random_uniform(UInt32(frame.width - floatingStartButton.frame.size.width))
+        let newY = arc4random_uniform(UInt32(frame.height - floatingStartButton.frame.size.height)) + UInt32(floatingStartButton.frame.size.height)
+        let newPoint = CGPoint(x: CGFloat(newX), y: CGFloat(newY))*/
+        var actionList = SKAction.sequence(
+            [SKAction.fadeIn(withDuration: 2.0),
+            SKAction.fadeOut(withDuration: 2.0),
+            SKAction.run(moveLabel)]
+        )
+        floatingStartButton.run(SKAction.repeatForever(actionList))
+    }
+    
+    func moveLabel(){
+        let newX = arc4random_uniform(UInt32(self.frame.width - self.floatingStartButton.frame.size.width))
+        let newY = arc4random_uniform(UInt32(self.frame.height - self.floatingStartButton.frame.size.height)) + UInt32(self.floatingStartButton.frame.size.height)
+        let newPoint = CGPoint(x: CGFloat(newX), y: CGFloat(newY))
+        self.floatingStartButton.position = newPoint
+        
     }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("frame size",frame.maxX,frame.maxY)
-        
-        var x = arc4random_uniform(UInt32(frame.width - floatingStartButton.frame.size.width))
-        var y = Int(arc4random_uniform(UInt32(frame.height - floatingStartButton.frame.size.height))) + Int(floatingStartButton.frame.size.height)
-        floatingStartButton.position.x = CGFloat(x)
-        floatingStartButton.position.y = CGFloat(y)
-        //var y = CGFloat(arc4random_uniform(UInt32(frame.maxY)))
-        
-        print (floatingStartButton.position.x,floatingStartButton.position.y)
-        
-        /*floatingStartButton.position = CGPoint(
-            x: CGFloat(arc4random_uniform(UInt32(frame.maxX - floatingStartButton.frame.maxX))),
-            y: CGFloat(arc4random_uniform(UInt32(frame.maxY)))
-        )*/
-        /*for t in touches {
+        for t in touches {
             let nodesAtLocation = self.nodes(at: t.location(in: self))
             for nodes in nodesAtLocation{
                 if nodes.name == "FloatingStartGameButton" {
@@ -52,7 +56,7 @@ class GameScene: SKScene {
                     break
                 }
             }
-        }*/
+        }
     }
     /*override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("test")
