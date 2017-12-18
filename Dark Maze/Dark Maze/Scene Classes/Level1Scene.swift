9374374498aa77afd38a427f728c60090027b93f
crossibc <Administrator@iMac.local>
2017-12-17 21:49:49 -0500

    created part of a grid

--- Dark Maze/Dark Maze/Scene Classes/Level1Scene.swift
+++ Dark Maze/Dark Maze/Scene Classes/Level1Scene.swift
@@ -9,6 +9,34 @@
 import SpriteKit
 
 class Level1Scene: SKScene {
+    let gridSize = 4
+    let blockBuffer = 2
     
+    override func didMove(to view: SKView) {
+        initializeGrid()
+    }
+    /*override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
+    }*/
+    private func initializeGrid(){
+        let blocksize = self.frame.maxX / CGFloat(gridSize + blockBuffer)
+        
+        
+        for index in (blockBuffer-1)...gridSize{
+            let cgI = CGFloat(index)
+            var points = [CGPoint(x:cgI * blocksize, y: 400),
+                          CGPoint(x:cgI * blocksize + blocksize, y: 400),
+                          CGPoint(x:cgI * blocksize + blocksize, y: 400-blocksize),
+                          CGPoint(x:cgI * blocksize, y: 400-blocksize),
+                          CGPoint(x:cgI * blocksize, y: 400)
+            ]
+
+            var tile = SKShapeNode(points: &points, count: points.count)
+            tile.lineWidth = 2
+            tile.glowWidth = 2
+            self.addChild(tile)
+        }
+
+        //tempNode.run(action)
+    }
 }
--- Dark Maze/Dark Maze/Scene Classes/LevelSelectScene.swift
+++ Dark Maze/Dark Maze/Scene Classes/LevelSelectScene.swift
@@ -1,5 +1,5 @@
 //
-//  Level1Scene.swift
+//  LevelSelectScene.swift
 //  Dark Maze
 //
 //  Created by crossibc on 12/5/17.
@@ -15,14 +15,11 @@ class LevelSelectScene: SKScene {
     private var label : SKLabelNode?
     
     override func didMove(to view: SKView) {
-        level1 = self.childNode(withName: "Level1") as? SKSpriteNode
-        level1?.position = CGPoint(x: frame.midX, y: frame.midY)
-        initialize
-        
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let gameSceneTemp = SKScene(fileNamed: "Levels1Scene")
         self.scene?.view?.presentScene(gameSceneTemp!, transition: SKTransition.doorsCloseHorizontal(withDuration: 2))
     }
+
 }
 
